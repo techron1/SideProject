2df410001ea4b5ed6094dc36f561c0a9958e8419
techron1 <64335250+techron1@users.noreply.github.com>
2020-04-26 00:56:59 -0700

    Project build config deploy style
    File Upload

---
+++ deploy.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+
+# This shell script will be part of jenkins with argument as buildName once stable to deploy new jobs
+
+# Install before running this
+
+DownloadedFileDir="/home/abc/gitClonedFileUntar/"
+JENKINS_DIR="/var/lib/Jenkins/jobs"
+JMETER_DIR="/home/abc/Tools/apache-jmeter-5.2.1/"
+buildName="FunctionalTest"
+BuildDir="$JENKINS_DIR/$buildName"
+mkdir -p $BuildDir
+
+# Script Dir
+BaseDir="/home/aaaa/RegressionTest"
+ScriptDir="$BaseDir/TestSuite1/"
+mkdir -p $ScriptDir
+
+
+mkdir -p $BaseDir/BuildFile
+mkdir -p $BaseDir/WorkSpace
+cp $DownloadedFileDir/build.xml $BaseDir/BuildFile/detectionBuild.xml
+
+
+
+# Copy the jenkins config from downloaded file
+cp $DownloadedFileDir/config.xml $BuildDir/config.xml
+cp $DownloadedFileDir/jmeterstyle.xml $JMETER_DIR/extras/jmeter-results-detail-report_211.xsl
+cp $DownloadedFileDir/project.jmx $ScriptDir/
+
+# Replace the variable from the current build file
+sed -i s/"BAS_DIR"/"$BaseDir"/g $BuildDir/config.xml
+sed -i s/"JM_INST_DIR"/"$JMETER_DIR"/g $BuildDir/config.xml
+sed -i s/"SCPT_DIR"/"$ScriptDir"/g $BuildDir/config.xml
+
+#Create empty file in jenkins needed
+touch $BuildDir/nextBuildNumber
+echo "1" > $BuildDir/nextBuildNumber
+mkdir -p $BuildDir/builds
+touch $BuildDir/builds/legacyIds
+touch $BuildDir/builds/permalinks
+
+# Ant build.xml
+
+echo "Open jenkins and run reload configuration from disk"
